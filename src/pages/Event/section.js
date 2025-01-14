e47ff952661b315472c52b8071612c968ce66b96
Manas Jain <jainmanas0410@gmail.com>
2025-01-14 00:09:47 -0600

    no bg primary

--- src/pages/Event/section.js
+++ src/pages/Event/section.js
@@ -47,7 +47,7 @@ class Section extends Component {
     return (
       <React.Fragment>
         <section
-          className="bg-half-260 bg-primary d-table w-100"
+          className="bg-half-260 d-table w-100"
           style={{
             background: `url(${bgImage}) center center`,
             backgroundSize: 'cover',
@@ -56,7 +56,7 @@ class Section extends Component {
         >
           <div
             className="bg-overlay bg-black bg-gradient"
-            style={{ opacity: '0.1' }}
+            style={{ opacity: '0.55' }}
           ></div>
           <Container>
             <Row className="justify-content-center">
