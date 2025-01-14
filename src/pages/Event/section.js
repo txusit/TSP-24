fbe8d6515c1f08a1b840092f468dc1e015da9e1d
Manas Jain <jainmanas0410@gmail.com>
2025-01-13 23:39:44 -0600

    jpg

--- src/pages/Event/section.js
+++ src/pages/Event/section.js
@@ -7,7 +7,7 @@ import { Container, Row, Col } from 'reactstrap';
 //CountDown
 import Countdown from 'react-countdown-now';
 
-import bgImage from '../../assets/images/event/black.png';
+import bgImage from '../../assets/images/event/TSP Foam Board.jpg';
 
 // Random component
 const Completionist = () => <span>You are good to go!</span>;
