4396201692f6e64a8451ff62c569c437cd119ce0
Manas Jain <jainmanas0410@gmail.com>
2025-01-26 13:18:29 -0600

    no spaces

--- src/pages/Event/section.js
+++ src/pages/Event/section.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
+// import { mdiArrowRightBoldBoxOutline } from '@mdi/js';
 
-// import { Parallax } from "react-parallax";
+// import { Parallax } from 'react-parallax';
 
 //CountDown
 import Countdown from 'react-countdown-now';
 
-import bgImage from '../../assets/images/event/Untitled_Artwork (2).png';
+import bgImage from '../../assets/images/event/bannerTry2.png';
 
 // Random component
-const Completionist = () => <span>You are good to go!</span>;
+const Completionist = () => <span></span>; //replace with countdown complete message
 
 // Renderer callback with condition
 const renderer = ({ days, hours, minutes, seconds, completed }) => {
@@ -47,81 +48,53 @@ class Section extends Component {
     return (
       <React.Fragment>
         <section
-          className="bg-half-260 d-table w-100"
-          style={{
-            background: `url(${bgImage}) center center`,
-            backgroundSize: 'cover',
-            backgroundRepeat: 'no-repeat',
-          }}
+          className="bg-half-260 w-100 d-table"
+          style={{ background: `url(${bgImage}) center center` }}
         >
           <div
             className="bg-overlay bg-black bg-gradient"
-            style={{ opacity: '0.55' }}
+            style={{ opacity: '0.50' }}
           ></div>
           <Container>
             <Row className="justify-content-center">
               <Col lg="10" className="text-center">
                 <div className="title-heading">
+                  {/* <h4 className="text-success mb-3">11th October, 2020</h4> */}
                   <h1 className="display-4 title-dark text-white fw-bold mb-3">
-                    Texas Stock Pitch
+                    2024 Texas Charity Pitch
                   </h1>
-                  <p className="para-desc title-dark mx-auto text-light">
-                    10:30 am CST
-                    <br />
-                    February 15th, 2025
-                  </p>
-
+                  <h4 className="para-desc title-dark mx-auto text-light">
+                    Building the Foundation for the Future
+                  </h4>
+                  <br></br>
                   <Row>
                     <Col md="12" className="text-center">
                       <div id="eventdown">
                         <Countdown
                           date={
                             Date.now() +
-                            Math.abs(new Date('2025/02/15 10:30') - Date.now())
+                            Math.abs(new Date('2024/11/09 08:00') - Date.now())
                           }
                           renderer={renderer}
                         />
                       </div>
                     </Col>
                   </Row>
-
-                  <div className="mt-4 pt-2">
-                    <Link
-                      to={{
-                        pathname: '/external-apply-form',
-                      }}
-                      target="_blank"
-                      className="btn btn-success mt-2 me-2"
-                    >
-                       Apply Now
+                  {/*<div className="mt-4 pt-2">
+                    <Link to="/external-apply-form" target="_blank" className="btn btn-primary mt-2 me-2">
+                      Apply to Pitch
                     </Link>
-                  </div>
+                        </div>*/}
                 </div>
               </Col>
             </Row>
           </Container>
         </section>
-        {/* <div className="position-relative">
+        <div className="position-relative">
           <div className="shape overflow-hidden text-footer">
             <div className="text-center bg-white p-4">
-              <h5 className="text-dark mb-0">
-                Competition Date : Feburary 19th, 2023
-              </h5>
+              <h5 className="text-dark mb-0">Competition Day : November 9th, 2024</h5>
             </div>
-          </div>
-        </div> */}
-        <div className="position-relative">
-          <div className="shape overflow-hidden text-white">
-            <svg
-              viewBox="0 0 2880 48"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z"
-                fill="currentColor"
-              ></path>
-            </svg>
           </div>
         </div>
         {/* </Parallax> */}
