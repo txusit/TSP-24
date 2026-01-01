2fb695112973918f8b4460d01bce47b06eb9025b
abbyshi7 <abby.shi@kinkaid.org>
2026-01-01 23:55:45 +1100

    Use HashRouter for GitHub Pages

--- src/App.js
+++ src/App.js
@@ -1,28 +1,26 @@
-import React, { Component, Suspense } from "react";
+// import React, { Component, Suspense } from "react";
 import Layout from "./components/Layout/";
 import {
   Route,
   Switch,
-  BrowserRouter as Router,
+  HashRouter as Router,
   withRouter,
 } from "react-router-dom";
 
 // Import Css
 import "./assets/css/materialdesignicons.min.css";
 import "./Apps.scss";
-
 import "./assets/css/colors/default.css";
 
 // Include Routes
 import routes from "./routes";
 
 function withLayout(WrappedComponent, hasDarkTopBar) {
-  // ...and returns another component...
   return class extends React.Component {
     render() {
       return (
         <Layout hasDarkTopBar={hasDarkTopBar}>
-          <WrappedComponent></WrappedComponent>
+          <WrappedComponent />
         </Layout>
       );
     }
@@ -42,6 +40,7 @@ class App extends Component {
       </div>
     );
   };
+
   render() {
     return (
       <React.Fragment>
@@ -51,16 +50,19 @@ class App extends Component {
               {routes.map((route, idx) =>
                 route.isWithoutLayout ? (
                   <Route
+                    exact
                     path={route.path}
-                    exact={route.exact}
                     component={route.component}
                     key={idx}
                   />
                 ) : (
                   <Route
-                    path={route.path}
                     exact
-                    component={withLayout(route.component, route.isTopbarDark)}
+                    path={route.path}
+                    component={withLayout(
+                      route.component,
+                      route.isTopbarDark
+                    )}
                     key={idx}
                   />
                 )
